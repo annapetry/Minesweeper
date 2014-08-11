b288a5b86a657fa61d3435be02f4764e06010e33
App Academy Student <student@appacademy.io>
2014-08-11 15:19:40 -0700

    Board sets bombs and values of positions

--- minesweeper/minesweeper.rb
+++ minesweeper/minesweeper.rb
@@ -5,63 +5,142 @@ end
 class Board
   attr_reader :board
   def initialize
-    @board = Array.new(9) { |i| Array.new(9) { |j| Tile.new(@board,[i,j]) }}
+    @board = Array.new(9) { |i| Array.new(9) { |j| Tile.new(@board, [i, j]) } }
     seed_bombs
+    
+    @board.each do |x|
+      x.each do |y|
+        neighbor_bombs(y.position)
+      end
+    end
+    
+    board_state
+    
   end
   
   def seed_bombs
     bomb_cords = []
     
-    until bomb_cords.count == 5
+    until bomb_cords.count == 20
       x = (0..8).to_a.sample
       y = (0..8).to_a.sample
       
       unless bomb_cords.include?([x, y])
         @board[x][y].bombed = true
+        @board[x][y].state = @board[x][y].set_state
+        @board[x][y].revealed = true        
+      
         bomb_cords << [x, y]
       end
-      
     end
     p bomb_cords
   end
   
-  def get_square(position)
-   @board[position[0]][positions[1]]
+  def board_state
+    @board.each do |x|
+      x.each do |y|
+        y.set_state
+      end
+    end
   end
+  
+  
+  def set_flag(position)
+    @board[position[0]][position[1]].flagged = true
+  end
+  
+  def neighbor_bombs(pos)
+    board_pos = @board[pos[0]][pos[1]]
+    adj_squares = board_pos.neighbors # this outputs a []
+    counter = 0
+    
+    adj_squares.each do |sq|
+      board_pos.value +=1 if @board[sq[0]][sq[1]].bombed
+    end
+    
+    # puts "adjacent suqared: #{adj_squares}"
+    board_state
+  end
+  
+  # def get_square(position)
+  #  @board[position[0]][positions[1]]
+  # end
 end
 
 class Player
+  def initialize
+  end
+  
+  def choice
+  end
+  
+  def set_a_flag #call set flag on board class
+  end
+  
+  def choose_a_square
+  end
   
 end
 
 class Tile
-  attr_accessor :board, :bombed
-  attr_reader :revealed, :flagged, :value, :position, :state
+  attr_accessor :board, :bombed, :revealed, :state, :flagged, :value
+  attr_reader :position
 
-  def initialize(board = Board.new, pos = nil, revealed = false, bombed = false, flagged = false, value = " ")
+  def initialize(board = Board.new, pos = nil)
     @board = board
     @position = pos 
-    @bombed = bombed
-    @revealed = revealed
-    @flagged = flagged
-    @value = value
-    @state =  set_state
+    @bombed = false
+    @revealed = false
+    @flagged = false
+    @value = 0
+    @state = 0
   end
 
   def set_state
-    return "*" if @revealed == false
+    # return "*" if @revealed == false
 
     if @bombed
-      return "B"
+      return @state = "B"
     elsif @flagged
-      return "F"
+      return @state = "F"
     else
-      return @value
+      return @state = @value
     end
 
   end
 
-
+  NEIGHBOR_TILES =[
+    [-1, 1],
+    [-1, 0],
+    [-1, -1],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [1, -1]
+  ]
+  
+  def neighbors
+    return @neighbors if @neighbors
+    @neighbors = []
+    
+    cur_x, cur_y = self.position
+    
+    NEIGHBOR_TILES.each do |(dx, dy)|
+      new_position = [cur_x + dx, cur_y + dy]
+      
+      if new_position.all? { |coord| coord.between?(0, 8) }
+        @neighbors << new_position
+      end
+    end
+    
+    @neighbors
+  end
+  
+  # def inspect
+  #
+  # end
+  
 end
 
 
@@ -70,6 +149,21 @@ def testing
   puts "Testing Board class..."
   puts "\n\nCreates new board and populates with Tiles"
   game_board = Board.new
+  
+  game_board.board.each do |row|
+    row.each do |col|
+       print "#{col.state} "
+    end
+    puts ""
+  end
+
+
+  
+  
+  puts "\nPrint out bomb coords"
+  # game_board.seed_bombs
+  
+  
   game_board.board.each do |row|
     row.each do |col|
        print "#{col.state} "
@@ -77,9 +171,7 @@ def testing
     puts ""
   end
   
-  puts "\nPrint out bomb coords"
-  game_board.seed_bombs
-  
+  p game_board.board
   
 end
 
